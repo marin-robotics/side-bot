772d40c5b60767cc7948eca001ec794cc9fd6e6e
MarinRobotics <25112590+MarinRobotics@users.noreply.github.com>
2020-01-24 19:05:49 -0800

    Basic-ish auton
    need to clean up comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include "main.h"
 
+#define pi 3.14159265359
+
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 pros::Controller partner(pros::E_CONTROLLER_PARTNER);
 pros::Motor back_left_drive(17);
 pros::Motor front_left_drive(19);
-pros::Motor back_right_drive(10, true);
-pros::Motor front_right_drive(1, true);
+pros::Motor back_right_drive(20, true);
+pros::Motor front_right_drive(10, true);
 
 pros::Motor lift(2);
 
-pros::Motor claw(18);
+pros::Motor claw(1);
 
 // Partner motors
 
@@ -17,11 +19,55 @@ pros::Motor partner_left_drive(8);
 pros::Motor partner_right_drive(9, true);
 
 /**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
+ * Blocks until the specified motors are done moving the specified distance (in inches) at the specified velocity.
+*/
+void move(pros::Motor motors[], int num_motors, double distance, int velocity) {
+	double ticks = distance * 225 / pi;
+
+	for (int i = 0; i < num_motors; i++) {
+		motors[i].tare_position();
+		motors[i].move_absolute(ticks, velocity);
+		pros::lcd::print(2 + i, "%f %f", ticks, motors[i].get_position());
+	}
+	pros::lcd::print(0, "NOT DONE");
+	bool all_motors_done = false;
+	while (!all_motors_done) {
+		for (int i = 0; i < num_motors; i++) {
+			// Check if the motor is not done moving (+ or - 5 units)
+			// i.e., if |distance - motor position| > 5
+			all_motors_done = true;
+			if (fabs(ticks - motors[i].get_position()) > 5) {
+				all_motors_done = false;
+				break;
+			}
+		}
+		pros::delay(2);
+	}
+	pros::lcd::print(0, "DONE");
+}
+
+/**
+ * Rotates the bot the specified number of degrees (can be positive or negative) at the specified velocity
+*/
+void rotateMain(double degrees, int velocity) {
+
+	double ticks = degrees * 32000 / 3555;
+
+	back_left_drive.tare_position();
+	front_left_drive.tare_position();
+	back_right_drive.tare_position();
+	front_right_drive.tare_position();
+
+	back_left_drive.move_absolute(ticks, velocity);
+	front_left_drive.move_absolute(ticks, velocity);
+	back_right_drive.move_absolute(-ticks, velocity);
+	front_right_drive.move_absolute(-ticks, velocity);
+	pros::lcd::print(1, "bro wth. %d, %d, %d, %d, %d", ticks, back_left_drive.get_position(), front_left_drive.get_position(), back_right_drive.get_position(), front_right_drive.get_position());
+	while (!((back_left_drive.get_position() < ticks + 5) && (back_left_drive.get_position() > ticks - 5))) {
+		pros::delay(2);
+	}
+}
+
 void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
@@ -75,14 +121,126 @@ void competition_initialize() {}
  * from where it left off.
  */
 void autonomous() {
-	// move forward
-	back_left_drive = 127, front_left_drive = 127, back_right_drive = 127, front_right_drive = 127;
-	pros::delay(1000);
-	// move backwards
-	back_left_drive = -127, front_left_drive = -127, back_right_drive = -127, front_right_drive = -127;
-	pros::delay(1000);
-	// stop movement
-	back_left_drive = 0, front_left_drive = 0, back_right_drive = 0, front_right_drive = 0;
+
+	// botMove(4, 127);
+
+	// rotateMain(360*10, 50);
+
+	front_left_drive = -127;
+	front_right_drive = -127;
+	back_left_drive = -127;
+	back_right_drive = -127;
+
+	pros::delay(250);
+
+	front_left_drive = 127;
+	front_right_drive = 127;
+	back_left_drive = 127;
+	back_right_drive = 127;
+
+	pros::delay(250);
+
+	front_left_drive = 80;
+	front_right_drive = 50;
+	back_left_drive = 80;
+	back_right_drive = 50;
+	claw = 40;
+
+	pros::delay(1500);
+
+	front_left_drive = 0;
+	front_right_drive = 0;
+	back_left_drive = 0;
+	back_right_drive = 0;
+	claw = -100;
+
+	pros::delay(500);
+
+	claw = -20;
+
+	front_left_drive = 127;
+	back_left_drive = 127;
+	front_right_drive = -127;
+	back_right_drive = -127;
+
+	pros::delay(1050);
+
+	front_left_drive = 0;
+	front_right_drive = 0;
+	back_left_drive = 0;
+	back_right_drive = 0;
+
+	pros::delay(500);
+
+	front_left_drive = 127;
+	front_right_drive = 127;
+	back_left_drive = 127;
+	back_right_drive = 127;
+
+	pros::delay(500);
+
+	front_left_drive = 0;
+	front_right_drive = 0;
+	back_left_drive = 0;
+	back_right_drive = 0;
+
+	/*pros::Motor motors[] = {front_left_drive, front_right_drive, back_left_drive, back_right_drive};
+	move(motors, 4, -3, 127);
+	pros::lcd::print(1, "WAITING");
+	pros::delay(20);
+	pros::lcd::print(1, "GOINg");
+	move(motors, 4, 3, 127);
+
+	pros::lcd::print(1, "1, jjkkkkkkkkk");
+	rotateMain(120, 100);
+	pros::delay(20);
+
+	pros::lcd::print(1, "JFJFJFFJFJFJFJFJF");
+	move(motors, 4, 20, 127);
+	pros::lcd::print(1, "jfjfjfjfjjfjfjf");
+	pros::delay(20);
+
+	pros::lcd::print(1, "gaming");
+	rotateMain(-110, 127);
+	pros::lcd::print(1, ".....");
+	pros::delay(200);
+
+	claw = 127;
+	pros::delay(2000);
+	claw = 0;
+
+	pros::delay(20);
+
+	move(motors, 4, 20, 127);
+	front_left_drive = 100;
+	back_left_drive = 100;
+	front_right_drive = 127;
+	back_right_drive = 127;
+
+	pros::delay(750);
+
+	front_left_drive = 0;
+	back_left_drive = 0;
+	front_right_drive = 0;
+	back_right_drive = 0;
+
+	pros::delay(20);
+
+	claw = -127;
+	pros::delay(1500);
+	claw = 0;
+
+	pros::delay(20);
+
+	rotateMain(-165, 127);
+
+	pros::delay(20);
+
+	move(motors, 4, 40, 127);
+
+	pros::delay(20);*/
+
+
 }
 
 /**
